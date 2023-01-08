28fc072a6ff3e1d25f31857cdf2e6960371401f2
Jaewon Chung <jaewon.chung.dev@gmail.com>
2023-01-08 23:40:49 +0900

    Add Items Add Controller and View Templates

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     /**
-     * Add Items for Test
+     * Set Basic Items
      */
     @PostConstruct
     public void init(){
@@ -43,6 +40,45 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam Integer price,
+            @RequestParam Integer quantity,
+            Model model
+    ){
+        Item savedItem = itemRepository.save(new Item(itemName, price, quantity));
+        model.addAttribute("item", savedItem);
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(
+            @ModelAttribute("item") Item item
+    ){
+        itemRepository.save(item);
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(
+            @ModelAttribute("item") Item item
+    ){
+        itemRepository.save(item);
+        return "/basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "/basic/item";
     }
 
 }
